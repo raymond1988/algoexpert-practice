ab358d7280447ffdfcc5b458e7d10c313cde5291
Raymond <raymanoharan@gmail.com>
2021-08-19 10:25:56 -0400

    mergeOverlappingIntervals

---
+++ mergeOverlappingIntervals.js
@@ -0,0 +1,30 @@
+let intervals = [
+  [1, 2],
+  [3, 5],
+  [4, 7],
+  [6, 8],
+  [9, 10]
+]
+
+function mergeOverlappingIntervals (intervals) {
+  const sortedIntervals = intervals.sort((a, b) => a[0] - b[0])
+
+  const mergedIntervals = []
+  let currentInterval = sortedIntervals[0]
+  mergedIntervals.push(currentInterval)
+
+  for (const nextInterval of sortedIntervals) {
+    const [_, currentIntervalEnd] = currentInterval
+    const [nextIntervalStart, nextIntervalEnd] = nextInterval
+
+    if (currentIntervalEnd >= nextIntervalStart)
+      currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd)
+    else {
+      currentInterval = nextInterval
+      mergedIntervals.push(currentInterval)
+    }
+  }
+  return mergedIntervals
+}
+console.log(intervals);
+console.log(mergeOverlappingIntervals(intervals));
