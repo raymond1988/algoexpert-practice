8e30df31ef0a1cc2109c410cb5d57e2bea0d3760
Raymond <raymanoharan@gmail.com>
2021-09-15 15:08:09 -0400

    main

--- levenshtein.js
+++ levenshtein.js
@@ -1,15 +1,15 @@
 function levenshteinDistance (str1, str2) {
   const edits = []
-  for (let i = 0; i < str2.length; i++) {
+  for (let i = 0; i < str2.length + 1; i++) {
     const row = []
-    for (let j = 0; j < str1.length; j++) {
+    for (let j = 0; j < str1.length + 1; j++) {
       row.push(j)
     }
     row[0] = i
     edits.push(row)
   }
-  for (let i = 1; i < str2.length; i++) {
-    for (let j = 1; j < str1.length; j++) {
+  for (let i = 1; i < str2.length + 1; i++) {
+    for (let j = 1; j < str1.length + 1; j++) {
       if (str2[i - 1] === str1[j - 1]) {
         edits[i][j] = edits[i - 1][j - 1]
       } else {
