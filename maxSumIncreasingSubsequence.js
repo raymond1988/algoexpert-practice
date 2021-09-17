b7d10d8e99ef4d34a29755daeed2f3d7b32aa235
Raymond <raymanoharan@gmail.com>
2021-09-17 11:20:49 -0400

    unknown

--- maxSumIncreasingSubsequence.js
+++ maxSumIncreasingSubsequence.js
@@ -1,27 +1,27 @@
 const maxSumIncreasingSubsequence = array => {
   const sequenceTable = new Array(array.length)
-  const sum = array.map(num => num)
-  let maxSumIdx = 0
+  const sums = array.map(num => num)
+  let maxsumsIdx = 0
 
   for (let i = 0; i < array.length; i++) {
     const currentNum = array[i]
     for (let j = 0; j < i; j++) {
       const otherNum = array[j]
-      if (otherNum < currentNum && sum[j] + currentNum >= sum[i]) {
-        sum[i] = sum[j] + currentNum
+      if (otherNum < currentNum && sums[j] + currentNum >= sums[i]) {
+        sums[i] = sums[j] + currentNum
         sequenceTable[i] = j
       }
     }
-    if (sum[i] >= sum[maxSumIdx]) maxSumIdx = i
+    if (sums[i] >= sums[maxsumsIdx]) maxsumsIdx = i
   }
-  return [sum[maxSumIdx], buildSequence(array, sequenceTable, maxSumIdx)]
+  return [sums[maxsumsIdx], buildSequence(array, sequenceTable, maxsumsIdx)]
 }
 
 const buildSequence = (array, sequences, currentIdx) => {
   const sequence = []
   while (currentIdx !== undefined) {
     sequence.unshift(array[currentIdx])
-    currentIdx = sequence[currentIdx]
+    currentIdx = sequences[currentIdx]
   }
   return sequence
 }
