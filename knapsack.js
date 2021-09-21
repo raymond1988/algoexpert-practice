e4cabbae48df52bd9f00ca85c3c3535a1d1ba0bf
Raymond <raymanoharan@gmail.com>
2021-09-21 11:03:04 -0400

    function definiton commit

---
+++ knapsack.js
@@ -0,0 +1,15 @@
+const knapsackProblem = (items, capacity) => {
+    
+}
+
+console.log(
+  knapsackProblem(
+    [
+      [1, 2],
+      [4, 3],
+      [5, 6],
+      [6, 7]
+    ],
+    10
+  )
+)
