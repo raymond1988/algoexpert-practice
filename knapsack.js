2ef59b2c263b2e6c428bf6a54ff5ba2e388dc153
Raymond <raymanoharan@gmail.com>
2021-09-21 13:02:25 -0400

    inserted additional comments

--- knapsack.js
+++ knapsack.js
@@ -1,5 +1,48 @@
 const knapsackProblem = (items, capacity) => {
-    
+  const knapsackValues = []
+  for (let i = 0; i <= items.length; i++) {
+    const row = new Array(capacity + 1).fill(0)
+    knapsackValues.push(row)
+  }
+
+  for (let i = 1; i <= items.length; i++) {
+    const currentWeight = items[i - 1][1] // 0 index
+    const currentValue = items[i - 1][0] //0 index
+    for (let c = 0; c <= capacity; c++) {
+      if (currentWeight > c) {
+          //whatever that in the above row
+        knapsackValues[i][c] = knapsackValues[i - 1][c]
+      } else {
+          //get the max value btwn the value right above
+          //or the value of the prev insertion plus current
+        knapsackValues[i][c] = Math.max(
+          knapsackValues[i - 1][c],
+          knapsackValues[i - 1][c - currentWeight] + currentValue
+        )
+      }
+    }
+  }
+  return [
+    knapsackValues[items.length][capacity],
+    getKnapsackItems(knapsackValues, items)
+  ]
+}
+
+const getKnapsackItems = (knapsackValues, items) => {
+  const sequence = []
+  let i = knapsackValues - 1
+  let c = knapsackValues[0].length - 1
+  while (i > 0) {
+    if (knapsackValues[i][c] === knapsackValues[i - 1][c]) {
+      i -= 1
+    } else {
+      sequence.unshift(i - 1)
+      c -= items[i - 1][1]
+      i -= 1
+    }
+    if (c === 0) break
+  }
+  return sequence
 }
 
 console.log(
