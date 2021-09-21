9ca34fb9eb57c48e04faca3567dc93befa2ba5fe
Raymond <raymanoharan@gmail.com>
2021-09-21 16:06:25 -0400

    final commit for diskStacking

--- diskStacking.js
+++ diskStacking.js
@@ -1,7 +1,40 @@
 const diskStacking = disks => {
-    
+  disks.sort((a, b) => a[2] - b[2])
+  const heights = disks.map(disk => disks[2])
+  const sequences = new Array(disks.length)
+  let maxHeightIdx = 0
+  for (let i = 1; i < disks.length; i++) {
+    const currentDisk = disks[i]
+    for (let j = 0; j < i; j++) {
+      const otherDisk = disks[j]
+      if (areValidDimensions(otherDisk, currentDisk)) {
+        if (heights[i] <= currentDisk[2] + heights[j]) {
+          heights[i] = currentDisk[2] + heights[j]
+          sequences[i] = j
+        }
+      }
+    }
+    if (heights[i] >= heights[maxHeightIdx]) maxHeightIdx = i
+  }
+  return buildSequence(disks, sequences, maxHeightIdx)
 }
 
+const areValidDimensions = (otherDisk, currentDisk) => {
+  return (
+    otherDisk[2] < currentDisk[2] &&
+    otherDisk[1] < currentDisk[1] &&
+    otherDisk[0] < currentDisk[0]
+  )
+}
+
+const buildSequence = (array, sequences, currentIdx) => {
+  const sequence = []
+  while (currentIdx !== undefined) {
+    sequence.unshift(array[currentIdx])
+    currentIdx = sequences[currentIdx]
+  }
+  return sequence
+}
 console.log(
   diskStacking([
     [2, 1, 2],
