8e1160ec9b736c8f195e3c0de1e2d57f5fcd7813
Raymond <raymanoharan@gmail.com>
2021-09-22 12:28:08 -0400

    optimized version

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,8 +1,22 @@
 // O(n^2) - time O(n) - space
-const isPalindrome = string => {
+/* const isPalindrome = string => {
   let reverseString = ''
   for (let i = string.length - 1; i >= 0; i--) {
     reverseString += string[i]
   }
   return string === reverseString
 }
+ */
+//O(n) - time O(1) - space
+const isPalindrome = string => {
+  let leftPtr = string[0]
+  let rightPtr = string.length - 1
+  while (leftPtr < rightPtr) {
+    if (string[leftPtr] !== string[rightPtr]) return false
+    leftPtr++
+    rightPtr--
+  }
+  return true
+}
+
+console.log(isPalindrome("abcdedcba"));
