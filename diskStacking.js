811c1120ae00c5b3c8af40d9a565ab5522975c8b
Raymond <raymanoharan@gmail.com>
2021-09-22 05:53:30 -0400

    error fix

--- diskStacking.js
+++ diskStacking.js
@@ -2,7 +2,7 @@
 
 const diskStacking = disks => {
   disks.sort((a, b) => a[2] - b[2])
-  const heights = disks.map(disk => disks[2])
+  const heights = disks.map(disk => disk[2])
   const sequences = new Array(disks.length)
   let maxHeightIdx = 0
   for (let i = 1; i < disks.length; i++) {
