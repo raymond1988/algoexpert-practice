1913a6788d21f7e8f9c0b25f0e93102f3f317b23
Raymond <raymanoharan@gmail.com>
2021-09-27 15:46:49 -0400

    returns wrong result. needs improvement

---
+++ validIPAdresses.js
@@ -0,0 +1,38 @@
+const validIPAddress = string => {
+  const ipAddressFound = []
+
+  for (let i = 0; i < Math.min(string.length, 4); i++) {
+    const currentIPAddressParts = ['', '', '', '']
+
+    currentIPAddressParts[0] = string.slice(0, i)
+    if (!isValidPart(currentIPAddressParts[0])) continue
+
+    for (let j = i + 1; j < i + Math.min(string.length - i, 4); j++) {
+      currentIPAddressParts[1] = string.slice(i, j)
+      if (!isValidPart(currentIPAddressParts[1])) continue
+
+      for (let k = j + 1; k < j + Math.min(string.length - j, 4); k++) {
+        currentIPAddressParts[2] = string.slice(j, k)
+        currentIPAddressParts[3] = string.slice(k)
+
+        if (
+          isValidPart(
+            currentIPAddressParts[2] && isValidPart(currentIPAddressParts[3])
+          )
+        ) {
+          ipAddressFound.push(currentIPAddressParts.join('.'))
+        }
+      }
+    }
+  }
+  return ipAddressFound
+}
+
+const isValidPart = string => {
+  const stringAsInt = parseInt(string)
+  if (stringAsInt > 255) return false
+
+  return string.length === stringAsInt.toString().length
+}
+
+console.log(validIPAddress('1921680'))
