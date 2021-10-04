eaff7ce2fed4c01988d4e2a1fccde22678c63b4e
Raymond <raymanoharan@gmail.com>
2021-10-04 11:57:30 -0400

    commit

---
+++ getPermutations.js
@@ -0,0 +1,19 @@
+const getPermutations = array => {
+  const permutations = []
+  permutationsHelper(array, [], permutations)
+  return permutations
+}
+
+const permutationsHelper = (array, currentPermutation, permutations) => {
+  if (!array.length && currentPermutation.length) {
+    permutations.push(currentPermutation)
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      const newArray = array.slice(0, i).concat(array.slice(i + 1))
+      const newPermutation = currentPermutation.concat([array[i]])
+      permutationsHelper(newArray, newPermutation, permutations)
+    }
+  }
+}
+
+console.log(getPermutations([1, 2, 3, 4, 5]))
--- productSum.js
+++ productSum.js
@@ -1,3 +1,13 @@
-const productSum = () => {}
+const productSum = (array, multiplier = 1) => {
+  let sum = 0
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      sum += productSum(element, multiplier + 1)
+    } else {
+      sum += element
+    }
+  }
+  return sum * multiplier
+}
 
 console.log(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]]))
