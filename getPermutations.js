8ca7830633d3097fc0815b047e4c60310b7d91ff
Raymond <raymanoharan@gmail.com>
2021-10-04 20:39:25 -0400

    time complexity updated fn

--- getPermutations.js
+++ getPermutations.js
@@ -1,4 +1,4 @@
-const getPermutations = array => {
+/* const getPermutations = array => {
   const permutations = []
   permutationsHelper(array, [], permutations)
   return permutations
@@ -14,6 +14,30 @@ const permutationsHelper = (array, currentPermutation, permutations) => {
       permutationsHelper(newArray, newPermutation, permutations)
     }
   }
+} */
+
+//O(n*n!) - t and s
+const getPermutations = array => {
+  const permutations = []
+  permutationsHelper(0, array, permutations)
+  return permutations
 }
 
+const permutationsHelper = (i, array, permutations) => {
+  if (i === array.length - 1) {
+    permutations.push(array.slice())
+  } else {
+    for (let j = i; j < array.length; j++) {
+      swap(i, j, array)
+      permutationsHelper(i + 1, array, permutations)
+      swap(i, j, array)
+    }
+  }
+}
+
+const swap = (i, j, array) => {
+  const temp = array[i]
+  array[i] = array[j]
+  array[j] = temp
+}
 console.log(getPermutations([1, 2, 3]))
