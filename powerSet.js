7c3e8e749ed685679ee95012e024f2748f7fcf11
Raymond <raymanoharan@gmail.com>
2021-10-06 10:12:11 -0400

    combinations commit

--- powerSet.js
+++ powerSet.js
@@ -1,5 +1,5 @@
 //O(n*2^n) - t | O(n*2^n) - s
-
+/* 
 const powerSet = (array, idx = null) => {
   if (idx === null) {
     idx = array.length - 1
@@ -14,8 +14,29 @@ const powerSet = (array, idx = null) => {
     subsets.push(currentSubset.concat(ele))
   }
   return subsets
-}
+} */
 
-console.log(powerSet([1, 2, 3]))
+/* const combinations = elements => {
+  //base case
+  if (elements.length === 0) return [[]]
+  //get the first element - we're trying to shrink the array
+  //progressing towards the base case
+  const firstEl = elements[0]
+  //generate the shrikned array
+  const rest = elements.slice(1)
+  //repeat till you hit the base case by recursion
+  const combinationsWithoutFirst = combinations(rest)
+  const combinationsWithFirst = []
+
+  combinationsWithoutFirst.forEach(comb => {
+    const combinationWithFirst = [...comb, firstEl]
+    combinationsWithFirst.push(combinationWithFirst)
+  })
+
+  return [...combinationsWithoutFirst, ...combinationsWithFirst]
+} */
 
 
+
+//console.log(powerSet(['a','b','c']))
+console.log(combinations(['a', 'b', 'c']))
