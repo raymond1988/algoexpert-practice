3040cde9972b9aebf93c83353a141b275045e517
Raymond <raymanoharan@gmail.com>
2021-10-16 15:21:28 -0400

    first commit

---
+++ hasSingleCycle.js
@@ -0,0 +1,18 @@
+const hasSingleCycle = array => {
+  let numElementsVisited = 0
+  let currentIdx = 0
+  while (numElementsVisited < array.length) {
+    if (numElementsVisited > 0 && currentIdx === 0) return false
+    numElementsVisited++
+    currentIdx = getNextIdx(currentIdx, array)
+  }
+  return currentIdx === 0
+}
+
+const getNextIdx = (currentIdx, array) => {
+  const jump = array[currentIdx]
+  const nextIdx = (currentIdx + jump) % array.length
+  return nextIdx >= 0 ? nextIdx : nextIdx + array.length
+}
+
+console.log(hasSingleCycle([2, 3, 1, -4, -4, 2]))
