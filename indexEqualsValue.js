1bf9d58ad2e464ec8687e8a0deecd7df43cfa51b
Raymond <raymanoharan@gmail.com>
2021-11-16 12:00:41 -0500

    set up console

--- indexEqualsValue.js
+++ indexEqualsValue.js
@@ -2,14 +2,17 @@
 //the first index in the array that is equal to the value at that index.
 
 //O(n) t && O(n) s
-const indexEqualsValue = (array) => {
+const indexEqualsValue = array => {
   for (let index = 0; index < array.length; index++) {
-      const element = array[index];
-      if(index === element) return index
+    const element = array[index]
+    if (index === element) return index
   }
   return -1
 }
 
-const indexEqualsValue = () => {
-  
+const indexEqualsValue = array => {
+    
 }
+
+let array = [-5, -3, 0, 3, 4, 5, 9] //sorted array
+console.log(indexEqualsValue(array));
