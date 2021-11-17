1a8a837a742a13e488d13b30529440aa7de57a12
Raymond <raymanoharan@gmail.com>
2021-11-16 20:53:39 -0500

    updated function

--- indexEqualsValue.js
+++ indexEqualsValue.js
@@ -15,8 +15,10 @@ const indexEqualsValue = array => {
   return indexEqualsValueHelper(array, 0, array.length - 1)
 }
 
-const indexEqualsValueHelper = (array, leftIdx, rightIdx) => {
-  const middle = Math.floor((left + right) / 2)
+/* const indexEqualsValueHelper = (array, leftIdx, rightIdx) => {
+  if (leftIdx > rightIdx) return -1
+
+  const middle = leftIdx + Math.floor((leftIdx + rightIdx) / 2)
   const potentialMatch = array[middle]
   if (potentialMatch === middle && middle === 0) {
     return middle
@@ -26,6 +28,29 @@ const indexEqualsValueHelper = (array, leftIdx, rightIdx) => {
   }
   if (potentialMatch === middle && middle - 1 === array[middle - 1]) {
     indexEqualsValueHelper(array, 0, middle - 1)
+  }
+  if (potentialMatch > middle) {
+    indexEqualsValueHelper(array, middle + 1, rightIdx)
+  }
+} */
+
+const indexEqualsValueHelper = (array, leftIdx, rightIdx) => {
+  if (leftIdx > rightIdx) return -1
+
+  const middleIdx = leftIdx + Math.floor((rightIdx - leftIdx) / 2)
+  const middleValue = array[middleIdx]
+
+  if (middleValue < middleIdx) {
+    return indexEqualsValueHelper(array, middleIdx + 1, rightIdx)
+  } else if (middleValue === middleIdx && middleIdx === 0) {
+    return middleIdx
+  } else if (
+    middleValue === middleIdx &&
+    array[middleIdx - 1] < middleIdx - 1
+  ) {
+    return middleIdx
+  } else {
+    return indexEqualsValueHelper(array, leftIdx, middleIdx - 1)
   }
 }
 
