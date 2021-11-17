97bb73fc306015357d3eaf24958bdc9776e55a0b
Raymond <raymanoharan@gmail.com>
2021-11-17 11:55:22 -0500

    iterative version

--- binarySearch.js
+++ binarySearch.js
@@ -2,6 +2,7 @@ const binarySearch = (array, target) => {
   return binarySearchHelper(array, target, 0, array.length - 1)
 }
 
+/* //O(log(n)) - t & s
 const binarySearchHelper = (array, target, left, right) => {
   if (left > right) return -1
   const middle = Math.floor((left + right) / 2)
@@ -14,6 +15,22 @@ const binarySearchHelper = (array, target, left, right) => {
   } else {
     return binarySearchHelper(array, target, middle + 1, right)
   }
+} */
+
+//O(log(n)) - t and O(1) space
+const binarySearchHelper = (array, target, left, right) => {
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2)
+    const potentialMatch = array[middle]
+    if (target === potentialMatch) {
+      return middle
+    } else if (target < potentialMatch) {
+      right = middle - 1
+    } else {
+      left = middle + 1
+    }
+  }
+  return -1
 }
 
 console.log(binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33))
