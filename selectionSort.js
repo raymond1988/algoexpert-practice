fd7cd1e5f81586bd1eef6b335e4f21427b242649
Raymond <raymanoharan@gmail.com>
2021-11-18 11:42:20 -0500

    commit

---
+++ selectionSort.js
@@ -0,0 +1,18 @@
+const selectionSort = array => {
+  let startIdx = 0
+  while (startIdx < array.length - 1) {
+    let smallestIdx = startIdx
+    for (let i = startIdx + 1; i < array.length; i++) {
+      if (array[smallestIdx] > array[i]) smallestIdx = i
+    }
+    swap(startIdx, smallestIdx, array)
+    startIdx++
+  }
+  return array
+}
+
+const swap = (i, j, array) => {
+  const temp = array[j]
+  array[j] = array[i]
+  array[i] = temp
+}
